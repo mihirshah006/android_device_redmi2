f05682e90db6df3b15d2031c350a5407faa8d20c
ssawrav <s.sawrav@gmail.com>
2016-10-10 20:30:51 +0530

    Zephyr-N Bringup

--- zos_wt88047.mk
+++ zos_wt88047.mk
@@ -15,8 +15,8 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Zephyr stuff.
+$(call inherit-product, vendor/zos/common.mk)
 
 # Inherit from wt88047 device
 $(call inherit-product, device/wingtech/wt88047/device.mk)
@@ -28,8 +28,10 @@ TARGET_BOARD_PLATFORM_VARIANT := msm8916
 BOARD_VENDOR := wingtech
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := wt88047
-PRODUCT_NAME := cm_wt88047
+PRODUCT_NAME := zos_wt88047
 PRODUCT_MANUFACTURER := Wingtech
 PRODUCT_MODEL := Redmi 2
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += DEVICE_MAINTAINERS="Saket Sawrav"
